14c79b43cfaeb768cc3971bb2f20ed115b3cad76
vibhu Bhan <39630233+hiwaveSupport@users.noreply.github.com>
2023-03-19 20:19:34 -0400

    with gpu test code

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
 from pip._internal.operations.freeze import freeze
 for requirement in freeze(local_only=True):
     print(requirement)
+
+# setting device on GPU if available, else CPU
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Using device:', device)
+print()
+
+
+#Additional Info when using cuda
+if device.type == 'cuda':
+    print(torch.cuda.get_device_name(0))
+    print('Memory Usage:')
+    print('Allocated:', round(torch.cuda.memory_allocated(0)/1024**3,1), 'GB')
+    print('Cached:   ', round(torch.cuda.memory_cached(0)/1024**3,1), 'GB')
